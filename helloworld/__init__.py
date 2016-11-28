bdd587d46ce2a90596b9d277a55fe74fb0eb4cde
Wes Mason <wes@1stvamp.org>
2016-11-28 14:34:23 +0000

    Add DB functionality from Caio's dummy app, and add docker building/pushing to travis

--- helloworld/__init__.py
+++ helloworld/__init__.py
@@ -1,6 +1,9 @@
 from logging import getLogger
 
-from flask import Flask
+from flask import Flask, request
+
+from helloworld.db import wrap_with_connection
+from helloworld.models import User
 
 
 app = Flask(__name__)
@@ -13,8 +16,27 @@ def hello_world():
     return 'Hello, World!'
 
 
+@app.route('/users/', methods=['POST'])
+@wrap_with_connection(app)
+def username(db):
+    user = User(request.form['username'])
+    db.session.add(user)
+    db.session.commit()
+    logger.info('username: {}'.format(request.form['username']))
+    return request.form['username']
+
+
+@app.route('/users/')
+@wrap_with_connection(app)
+def usernames(db):
+    users = User.query.all()
+    logger.info('users: {}'.format(users))
+    return str(users)
+
+
 def main():
-    app.run(host='0.0.0.0', port=8080)
+    debug = os.getenv('DEBUG', 'true').lower() == 'true'
+    app.run(debug=debug, host='0.0.0.0', port=8080)
 
 
 if __name__ == '__main__':
